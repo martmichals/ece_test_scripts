1e5c90d1fe3fdaedb24e42f1ba96c3990bafd242
Trevor Wong <txwong2@illinois.edu>
2020-02-21 19:52:09 -0600

    Updated autograder

--- autograder/autograde.py
+++ autograder/autograde.py
@@ -1,13 +1,71 @@
 #!/bin/python3
-import os,json,pexpect
+import os,json,sys,subprocess
 
-class_name = open('class.txt').read()
-repo_name = open('repo.txt').read()
+print('Python autograder started!')
+
+class_name = open('class.txt').read().strip()
+repo_name = open('repo.txt').read().strip()
+
+print('Searching for autograde scripts (class=%s, repo=%s)...'%(class_name, repo_name))
+
+scripts = []
 
 for (dirpath, dirnames, filenames) in os.walk('ece_test_scripts'):
 	for filename in filenames:
 		if filename == 'autograde.json':
-			
+			script = json.loads(open(dirpath + '/autograde.json').read())
+			if script['class'] == class_name:
+				script['path'] = dirpath
+				scripts += [script]
+
+if len(scripts) == 0:
+	print('No scripts found for this class! Aborting.')
+	sys.exit()
+
+exit = False
+
+while not exit:
+	selection = -1
+	print('What do you want to grade?')
+	for i in range(len(scripts)):
+		print('%d) %s'%(i+1, scripts[i]['name']))
+
+	while selection < 0 or selection >= len(scripts):
+		try:
+			selection = int(input('Selection: ')) - 1
+		except:
+			pass
+		if selection < 0 or selection >= len(scripts):
+			print('Invalid Selection!')
+
+	script = scripts[selection]
+	print('Ececuting %s...'%(script['name']))
+
+	if 'options' in script:
+		print('This script has multiple options.')
+		for i in range(len(script['options'])):
+			option = script['options'][i]
+			print('%d) %s - %s'%(i+1,option['display'],option['description']))
+		selection = -1
+		while selection < 0 or selection >= len(script['options']):
+			try:
+				selection = int(input('Selection ')) - 1
+			except:
+				pass
+			if selection < 0 or selection >= len(script['options']):
+				print('Invalid Selection!')
+		option = script['options'][selection]['option']
+		for i in range(len(script['commands'])):
+			script['commands'][i] = script['commands'][i].replace('{option}',option)
+
+
+	for in_file in script['files']:
+		subprocess.Popen(['cp',repo_name + '/' + in_file, script['path'] + '/']).wait()
 	
-print(class_name)
-print(repo_name)
+	os.chdir(script['path'])
+
+	for command in script['commands']:
+		subprocess.Popen(command, shell=True).wait()
+
+	if not input('Would you like to run another test [y/N]?').lower().contains('y'):
+		exit = True
