e4c9eb267942abc1f17d16455516b3c7fd513b35
Trevor Wong <txwong2@illinois.edu>
2020-03-02 13:02:31 -0600

    Fixed directory error

--- ece220_mp5/michalski_script/test_script.sh
+++ ece220_mp5/michalski_script/test_script.sh
@@ -1,6 +1,6 @@
 cd michalski_script
 
-gcc -g -std=c99 -Wall -Werror prog5.c c_runner.c -o c_runner
+gcc -g -std=c99 -Wall -Werror ../prog5.c ../c_runner.c -o c_runner
 echo "Running the test script!"
 
 python python_grader.py
